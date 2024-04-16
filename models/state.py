1e6738b4ceee86944c515044b9e36cd4d22a5b24
Lizzy0295 <lizzyjacinta95@gmail.com>
2024-04-16 09:00:17 -0700

    all tasks

--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
+from models.city import City
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ State class to add a new state object"""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade="all, delete-orphan")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """
+            Return the list of city objects from storage linked to the
+            current State
+            """
+            city_list = []
+            for city_obj in models.storage.all(City).values():
+                if city_obj.state_id == self.id:
+                    city_list.append(city_obj)
+            return city_list
